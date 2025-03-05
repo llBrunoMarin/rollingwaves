8829441d71fbda280307ae66ef15e27f36230c42
llBrunoMarin <llBrunoMarin@users.noreply.github.com>
2025-03-05 12:03:38 +0000

    Update page.js 288d389dd42c24fb6f37fd3e30e2455993726ca4

---
+++ _next/static/chunks/app/evaluation/page-2b1ca4d28b6d0208.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[750],{7761:function(n,e,u){Promise.resolve().then(u.bind(u,8413))}},function(n){n.O(0,[140,413,971,117,744],function(){return n(n.s=7761)}),_N_E=n.O()}]);
